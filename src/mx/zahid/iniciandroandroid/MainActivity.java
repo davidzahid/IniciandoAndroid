11aafcd0fb2d3a170770b0c3bb6c394153754db7
davidzahid <zahiddavid@gmail.com>
2014-04-10 18:51:13 -0500

    Second Test

--- src/mx/zahid/iniciandroandroid/MainActivity.java
+++ src/mx/zahid/iniciandroandroid/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//Enviando una cadena de texto a linearLayout con el metodo SetText
-		//prueba
+		//prueba 2
 		TextView titulo = (TextView) findViewById(R.id.titulo);
 		titulo.setText("Hola");
 	}
